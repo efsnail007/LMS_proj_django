4243c3001cdf4041f442211938070074a783761e
sasha <sgefsnkiospch908@gmail.com>
2024-04-06 15:54:54 +0300

    theme_14

--- project_tracker/project_tracker/settings.py
+++ project_tracker/project_tracker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "tasks.apps.TasksConfig"
+    "tasks.apps.TasksConfig",
 ]
 
 MIDDLEWARE = [
