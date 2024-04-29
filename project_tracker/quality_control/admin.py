eb113d0cf81a7a0cf3feb1551fdb824dc461a756
sasha <sgefsnkiospch908@gmail.com>
2024-04-29 08:49:38 +0300

    theme_16_dz

--- project_tracker/quality_control/admin.py
+++ project_tracker/quality_control/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import BugReport, FeatureRequest
+
+
+@admin.register(BugReport)
+class BugReportAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "project",
+        "task",
+        "status",
+        "priority",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "priority", "created_at", "updated_at")
+    search_fields = ("title", "description")
+    list_editable = ("status",)
+    fieldsets = [
+        (
+            "Основная информация",
+            {
+                "fields": [
+                    "title",
+                    "description",
+                    "project",
+                    "task",
+                    "status",
+                    "priority",
+                ]
+            },
+        ),
+    ]
+
+
+@admin.register(FeatureRequest)
+class FeatureRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "project",
+        "task",
+        "status",
+        "priority",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = ("status", "priority", "created_at", "updated_at")
+    search_fields = ("title", "description")
+    fieldsets = [
+        (
+            "Основная информация",
+            {
+                "fields": [
+                    "title",
+                    "description",
+                    "project",
+                    "task",
+                    "status",
+                    "priority",
+                ]
+            },
+        ),
+    ]
